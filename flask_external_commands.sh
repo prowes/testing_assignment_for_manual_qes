f5ce0b5bdd4fd3d0ebc4ce3a7a9ebd68d1b1c58a
Ivan Osipov <ivan.osipov@valagroup.com>
2023-10-09 16:19:33 +0300

    A script file for the gunicorn

---
+++ flask_external_commands.sh
@@ -0,0 +1,10 @@
+rm -r flapp
+mkdir flapp
+cd flapp
+python3 -m venv venv
+source venv/bin/activate
+git clone https://github.com/prowes/testing_assignment_for_manual_qes.git
+pip install -r /home/ec2-user/flapp/testing_assignment_for_manual_qes/requirements.txt
+export FLASK_APP=/home/ec2-user/flapp/testing_assignment_for_manual_qes/application.py
+cd testing_assignment_for_manual_qes
+gunicorn -b 0.0.0.0:8081 application:application
