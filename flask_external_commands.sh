d25714cf14a1bd8c6a3b058412caf04c3ea46325
Ivan Osipov <ivan.osipov@valagroup.com>
2023-10-10 16:41:11 +0300

    Add the service file for the ec2-user

--- flask_external_commands.sh
+++ flask_external_commands.sh
@@ -4,7 +4,7 @@ cd flapp
 python3 -m venv venv
 source venv/bin/activate
 git clone https://github.com/prowes/testing_assignment_for_manual_qes.git
-pip install -r /home/ec2-user/flapp/testing_assignment_for_manual_qes/requirements.txt
-export FLASK_APP=/home/ec2-user/flapp/testing_assignment_for_manual_qes/application.py
+pip install -r /var/lib/jenkins/workspace/WebApp/flapp/testing_assignment_for_manual_qes/requirements.txt
+export FLASK_APP=//var/lib/jenkins/workspace/WebApp/flapp/testing_assignment_for_manual_qes/application.py
 cd testing_assignment_for_manual_qes
 gunicorn -b 0.0.0.0:8081 application:application
