1d0d8f7343d7d56f68ee51cbb45ed51628b6317b
Ivan Osipov <ivan.osipov@valagroup.com>
2023-10-19 15:48:28 +0300

    Don't clone the repo again

--- commands/web_app_from_repo_jenkins_service.sh
+++ commands/web_app_from_repo_jenkins_service.sh
@@ -6,7 +6,7 @@ rm -f /etc/systemd/system/application_jenkins.service
 sudo systemctl daemon-reload
 
 # prepare all the env and stuff...
-cd flapp
+cd ../flapp
 python3 -m venv venv
 pip install -r /var/lib/jenkins/workspace/WebApp/flapp/testing_assignment_for_manual_qes/requirements.txt
 export FLASK_APP=/var/lib/jenkins/workspace/WebApp/flapp/testing_assignment_for_manual_qes/application.py
