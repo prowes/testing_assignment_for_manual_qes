06c44c5e4738b941f00de9ad079c8d0208fd14df
Ivan Osipov <ivan.osipov@valagroup.com>
2023-10-20 17:41:59 +0300

    Fix a title typo

--- flapp/application.py
+++ flapp/application.py
@@ -24,7 +24,7 @@ def enter():
         elif form.action.data == '\\':
             result = form.first_digit.data / form.second_digit.data
         return (f"Your result for {form.action.data} of {first} and {second} is {result}")
-    return (f'<html><head><title>Calculatr</title></head><body><form action="" method="post" novalidate>{form.hidden_tag()}<p>{form.first_digit.label}<br>{form.first_digit(size=7)}</p><p>{form.second_digit.label}<br>{ form.second_digit(size=6)}<p>{form.action.label}<br>{form.action(size=6)}</p><p>{form.submit()}</p></form></body></html>')
+    return (f'<html><head><title>Calculator</title></head><body><form action="" method="post" novalidate>{form.hidden_tag()}<p>{form.first_digit.label}<br>{form.first_digit(size=7)}</p><p>{form.second_digit.label}<br>{ form.second_digit(size=6)}<p>{form.action.label}<br>{form.action(size=6)}</p><p>{form.submit()}</p></form></body></html>')
 
 
 class EnterForm(FlaskForm):
