9b49f61bb7b3a81ab26dd2974444a7cb2bebfe5d
Ivan Osipov <ivan.osipov@valagroup.com>
2023-10-27 15:57:46 +0300

    Fix paths in the script

--- commands/flask_external_commands.sh
+++ commands/flask_external_commands.sh
@@ -1,10 +1,9 @@
-rm -r flapp
-mkdir flapp
-cd flapp
+rm -rf venv
+rm -rf testing_assignment_for_manual_qes
 python3 -m venv venv
 source venv/bin/activate
 git clone https://github.com/prowes/testing_assignment_for_manual_qes.git
-pip install -r /home/ec2-user/flapp/testing_assignment_for_manual_qes/requirements.txt
-export FLASK_APP=/home/ec2-user/flapp/testing_assignment_for_manual_qes/application.py
-cd testing_assignment_for_manual_qes
+pip install -r /home/ec2-user/testing_assignment_for_manual_qes/flapp/requirements.txt
+export FLASK_APP=/home/ec2-user/testing_assignment_for_manual_qes/flapp/application.py
+cd testing_assignment_for_manual_qes/flapp
 gunicorn -b 0.0.0.0:8081 application:application
